9b7ec44104d74d8d755f02080869f0ffe1206057
BoogieManN8 <tengizkiknadze8@gmail.com>
2024-03-24 21:18:07 +0400

    Add first onboarding screen

--- Bankey/OnBoardingViewController.swift
+++ Bankey/OnBoardingViewController.swift
@@ -7,34 +7,50 @@
 
 import UIKit
 
-//class OnBoardingViewController: UIView {
-//    override init(frame: CGRect){
-//        
-//        super.init(frame: frame)
-//        
-//        
-//        style()
-//        layout()
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    override var intrinsicContentSize: CGSize {
-//        return CGSize(width: 200, height: 200)
-//    }
-//}
-//
-//
-//extension OnBoardingViewController {
-//    
-//    func style(){
-//        translatesAutoresizingMaskIntoConstraints = false
-//    }
-//    
-//    func layout(){
-//        
-//    }
-//    
-//}
+class OnBoardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let label = UILabel()
+    let imageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnBoardingViewController {
+    func style(){
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        // Image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        
+        // Label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        
+    }
+    
+    func layout(){
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
